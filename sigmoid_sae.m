e95953f8b882d073318fc5b21dadb7ca7ea22e30
matthewkt-tang <matthewkt.tang@gmail.com>
2014-10-30 15:39:30 +1100

    sparsity
    added sparsity in sigmoid_sae.m
    (when called with true as 3rd argument, will apply 20% row sparsity to
    the input matrix)

--- sigmoid_sae.m
+++ sigmoid_sae.m
@@ -1,6 +1,14 @@
-function g = sigmoid_sae(x, gamma)
+function g = sigmoid_sae(x, gamma, sparse)
 % calculate the sigmoid function
 if nargin < 2
     gamma = 1;
 end
+if nargin < 3
+    sparse = false;
+end
 g = 1 ./ (1 + exp(-gamma * x));
+
+if sparse
+    % apply 20% row sparsity to the input matrix g
+    g = g .* (g >= prctile(g,80,2)*ones(1,size(g,2)));
+end
